5a198992eeaf6163f23e114bbdf125f8bd09eb97
andrea2399 <92758544+andrea2399@users.noreply.github.com>
2024-07-09 20:12:52 +0200

    Update datasets.py

--- datasets.py
+++ datasets.py
@@ -18,27 +18,14 @@ class ImageDataset(data.Dataset):
         return len(self.imgpaths)
     
     def __getitem__(self, index, color_format='RGB'):
-        '''
-        img = Image.open(self.imgpaths[index])
-        img = img.convert(color_format)
-        if self.transform is not None:
-            img = self.transform(img)
-        return img
-        '''
         # Path for CBCT image
         cbct_img_path = self.imgpaths[index]
         # Path for corresponding CT image
-        #ct_img_path = cbct_img_path.replace('_cbct.png', '_ct.png')
-        # Path for corresponding CT image
         ct_img_path = cbct_img_path.replace('_cbct.tif', '_ct.tif')
-        #print(f"Loading CBCT image from path: {cbct_img_path}")
-        #print(f"Loading CT image from path: {ct_img_path}")
-        #cbct_img = Image.open(cbct_img_path).convert('L')  # Convert to grayscale
-        #ct_img = Image.open(ct_img_path).convert('L')      # Convert to grayscale
-        
-        # Open the images using tifffile and convert to float32
-        cbct_img = tiff.imread(cbct_img_path)#.astype(np.float32)
-        ct_img = tiff.imread(ct_img_path)#.astype(np.float32)
+ 
+        # Open the images using tifffile
+        cbct_img = tiff.imread(cbct_img_path)
+        ct_img = tiff.imread(ct_img_path)
         cbct_img_1 = Image.fromarray(cbct_img)
         ct_img_1 = Image.fromarray(ct_img)
         if self.transform is not None:
@@ -46,28 +33,18 @@ class ImageDataset(data.Dataset):
             ct_img_1 = self.transform(ct_img_1)       
         cbct_array = np.array(cbct_img_1)
         ct_array = np.array(ct_img_1)
-        #cbct_array = np.array(cbct_img)
-        #ct_array = np.array(ct_img)
-        stacked_img = np.stack([cbct_array, ct_array], axis=-1)#.squeeze(0)
-        #stacked_img = stacked_img.permute(0, 3, 1, 2)
-        #print(stacked_img.shape)
-        # Convert the numpy array back to PIL Image
-        #stacked_img = Image.fromarray(stacked_img)
+        stacked_img = np.stack([cbct_array, ct_array], axis=-1)
         
         # Ensure the final output is a torch.Tensor
         if not isinstance(stacked_img, torch.Tensor):
             stacked_img = torch.from_numpy(stacked_img)
             stacked_img = stacked_img.permute(2, 0, 1)             
-        #if self.transform is not None:
-            #stacked_img = self.transform(stacked_img)
 
         cbct_img = stacked_img
-        return cbct_img #,stacked_img
+        return cbct_img
         
     def __is_imgfile(self, filepath):
         filepath = os.path.expanduser(filepath)
-        #if os.path.isfile(filepath) and imghdr.what(filepath):
-            #return True
         if os.path.isfile(filepath) and filepath.endswith('.tif'):
             return True
         return False
