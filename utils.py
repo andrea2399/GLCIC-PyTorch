3a5ebdec8ac68bba540e32f87931132b1dcaf06f
andrea2399 <92758544+andrea2399@users.noreply.github.com>
2024-07-10 13:03:24 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -140,9 +140,9 @@ def poisson_blend(input, output, mask):
     for i in range(num_samples):
         dstimg = transforms.functional.to_pil_image(input[i])
         srcimg = transforms.functional.to_pil_image(output[i])
-        msk = transforms.functional.to_pil_image(mask[i])
+        msk = np.array(transforms.functional.to_pil_image(mask[i].squeeze(0)))
         # compute mask's center
-        #xs, ys = np.where(msk == 255)                
+        xs, ys = np.where(msk == 255)                
         xmin, xmax = min(xs), max(xs)
         ymin, ymax = min(ys), max(ys)
         center = ((xmax + xmin) // 2, (ymax + ymin) // 2)
